bbd24e2f3928a9e4a6772c3a9aab15653c68855f
Hannes Preishuber <hannesp@ppedv.de>
2019-08-27 16:36:47 +0200

    Simple ToList File Access

---
+++ WebformsMuc2019CS/Modul04/WebForm3.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm3.aspx.cs" Inherits="WebformsMuc2019CS.Modul04.WebForm3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>my simple todo</h1>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Button"  OnClick="Button1_Click"/>
+            <hr />
+            <ul>
+            <%for (int i = 0; i < ToDoListe.Count(); i++)
+                {%>
+                <li><%=ToDoListe[i] %></li>
+               <% } %></ul>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebformsMuc2019CS/Modul04/WebForm3.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsMuc2019CS.Modul04
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        public string[] ToDoListe { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack==false)
+            {
+                ladeTodos();
+            }
+          
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            File.AppendAllText(Server.MapPath(@"~\app_data\todoitems.txt"), TextBox1.Text +
+                Environment.NewLine);
+            ladeTodos();
+        }
+        private void ladeTodos()
+        {
+            ToDoListe = File.ReadAllLines(Server.MapPath(@"~\app_data\todoitems.txt"));
+        }
+    }
+}
