f71eebc11611e9b5168984aae823c4e8969f638b
Hannes Preishuber <hannesp@ppedv.de>
2019-08-27 16:20:03 +0200

    Excel export

---
+++ WebformsMuc2019CS/Modul04/WebForm2.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebformsMuc2019CS.Modul04.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table>
+            <tr>
+                <th>Monat Name</th>
+
+            </tr>
+             <%foreach (var item in monate)
+             {%>
+            <tr>
+                <td><%=item %></td>
+             
+            </tr>
+
+             <%} %>
+
+        </table>
+  
+    </form>
+</body>
+</html>
---
+++ WebformsMuc2019CS/Modul04/WebForm2.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsMuc2019CS.Modul04
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+
+        public List<string> monate { get; set; } = new List<string>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            for (int i = 0; i <= 11; i++)
+            {
+                monate.Add( CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(i + 1));
+              
+            }
+            Response.ContentType = "application/vnd.ms-excel";
+        }
+    }
+}
