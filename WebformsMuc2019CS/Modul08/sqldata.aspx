d08683f4f62ed12e2392889bd88796a92684ce61
Hannes Preishuber <hannesp@ppedv.de>
2019-08-28 17:10:45 +0200

    Verschachtelte Repeater

---
+++ WebformsMuc2019CS/Modul08/sqldata.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="sqldata.aspx.cs" Inherits="WebformsMuc2019CS.Modul08.sqldata" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <ul class="list-group">
+        <asp:ListView ID="ListViewCity" runat="server" DataKeyNames="City" OnItemDataBound="ListViewCity_ItemDataBound">
+            <ItemTemplate>
+                <li class="list-group-item">
+                    <h2><%#Eval("City") %></h2>
+                    <span class="badge badge-primary float-right">
+                        <%#Eval("Anzahl") %></span>
+                    <ul>
+                        <asp:Repeater ID="rptKunden" runat="server">
+                            <ItemTemplate>
+                                <li> <%# Container.DataItem?.ToString() ?? string.Empty%></li>
+                            </ItemTemplate>
+                        </asp:Repeater>
+
+                    </ul>
+                </li>
+            </ItemTemplate>
+        </asp:ListView>
+
+
+    </ul>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ScriptPlaceHolder1" runat="server">
+</asp:Content>
---
+++ WebformsMuc2019CS/Modul08/sqldata.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsMuc2019CS.Modul08
+{
+    public partial class sqldata : System.Web.UI.Page
+    {
+        public List<string> liste { get; set; }= new List<string>();
+  
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var liste = new List<Cities>();
+
+            using (var con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["NorthwindConnectionString1"].ConnectionString))
+            {
+                var sql = "select city, count(*) as anzahl from customers group by city order by city";
+                //datatable ist bäh -generische Liste
+                var cmd = new SqlCommand(sql,con);
+                con.Open();
+                var rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                  
+                    liste.Add(new Cities { 
+                        Anzahl =int.Parse( rd["Anzahl"].ToString()),
+                        City = rd["City"].ToString() });
+                }
+
+            }
+            ListViewCity.DataSource = liste;
+            ListViewCity.DataBind();
+
+        }
+
+        
+        protected void ListViewCity_ItemDataBound(object sender, ListViewItemEventArgs e)
+        {
+            //e.Item.DataItem.City
+            liste.Clear();
+            using (var con = new SqlConnection(
+              ConfigurationManager.ConnectionStrings["NorthwindConnectionString1"].ConnectionString))
+            {
+                
+                var sql = "select companyname from customers where city='" + ((Cities)e.Item.DataItem).City + "'";
+
+                var cmd = new SqlCommand(sql, con);
+                con.Open();
+                var rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    liste.Add(rd.GetString(0));
+
+                }
+                var rpt = (Repeater)e.Item.FindControl("rptKunden");
+                rpt.DataSource = liste;
+                rpt.DataBind();
+
+            }
+
+
+
+
+        }
+    }
+    public class Cities
+    {
+        public string City { get; set; }
+        public int Anzahl { get; set; }
+    }
+}
