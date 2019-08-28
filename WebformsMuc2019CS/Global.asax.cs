28d525b54bc54b0269e189af21a22afffc861924
Hannes Preishuber <hannesp@ppedv.de>
2019-08-28 10:53:01 +0200

    Session Application Counter

--- WebformsMuc2019CS/Global.asax.cs
+++ WebformsMuc2019CS/Global.asax.cs
@@ -11,11 +11,24 @@ namespace WebformsMuc2019CS
 {
     public class Global : HttpApplication
     {
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            var u=(int)Application["users"];
+        
+            Application["users"] = ++u;
+        }
+        void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
         void Application_Start(object sender, EventArgs e)
         {
             // Code, der beim Anwendungsstart ausgeführt wird
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Application["users"] = 0;
         }
     }
 }
---
+++ WebformsMuc2019CS/Modul05/UserCounter.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UserCounter.aspx.cs" Inherits="WebformsMuc2019CS.Modul05.UserCounter" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Anzahl User</h1>
+        <div>
+           <%=Application["users"]%>
+        </div>
+    </form>
+</body>
+</html>
