8784226b14c7c795edda3f1de2bdce370ccd3d1b
Hannes Preishuber <hannesp@ppedv.de>
2019-08-29 15:50:27 +0200

    Valididierung

---
+++ WebformsMuc2019CS/Modul10/WebForm1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebformsMuc2019CS.Modul10.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="form-group row">
+        <div class="input-group mb-3">
+            <label for="exampleInputEmail1" class="col-sm-2 col-form-label">Company Name</label>
+            <div class="col-sm-10">
+                <asp:TextBox ID="TextBox1" runat="server" Text="" Class="form-control"></asp:TextBox>
+                <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" Display="Dynamic"
+                    CssClass="input-group-append"
+                    ControlToValidate="Textbox1"
+                    ErrorMessage="das ist keine email"
+                    ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*">*</asp:RegularExpressionValidator>
+                <asp:RequiredFieldValidator
+                    ControlToValidate="Textbox1"
+                    ID="RequiredFieldValidator1" runat="server" ErrorMessage="bitte füllen">*</asp:RequiredFieldValidator>
+            </div>
+            
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="exampleInputEmail1" class="col-sm-2 col-form-label">Alter</label>
+        <div class="col-sm-10">
+            <asp:TextBox ID="TextBox2" runat="server" Text="" Class="form-control"></asp:TextBox>
+            <asp:RangeValidator ID="RangeValidator1" runat="server" ErrorMessage="Altersfreigabe beachten" Display="Dynamic"
+                ControlToValidate="TextBox2" MaximumValue="80" MinimumValue="16">*</asp:RangeValidator>
+            <asp:RequiredFieldValidator
+                ControlToValidate="Textbox2"
+                ID="RequiredFieldValidator2" runat="server" ErrorMessage="bitte füllen">*</asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <asp:ValidationSummary ID="ValidationSummary1" runat="server" CssClass="alert alert-danger" />
+    <asp:Button Class="btn btn-light" ID="Button1" runat="server" Text="Cancel" CommandName="cancel" />
+    <asp:Button Class="btn btn-outline-primary" ID="Button2" runat="server" Text="save" CommandName="update" />
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ScriptPlaceHolder1" runat="server">
+</asp:Content>
---
+++ WebformsMuc2019CS/Modul10/WebForm1.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Dieser Code wurde von einem Tool generiert.
+//
+//     Änderungen an dieser Datei können fehlerhaftes Verhalten verursachen und gehen verloren, wenn
+//     der Code neu generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace WebformsMuc2019CS.Modul10 {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// TextBox1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// RegularExpressionValidator1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator1;
+        
+        /// <summary>
+        /// RequiredFieldValidator1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
+        /// TextBox2-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// RangeValidator1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator RangeValidator1;
+        
+        /// <summary>
+        /// RequiredFieldValidator2-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+        
+        /// <summary>
+        /// ValidationSummary1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
+        
+        /// <summary>
+        /// Button1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Button2-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+    }
+}
