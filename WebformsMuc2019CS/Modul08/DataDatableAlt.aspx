3e4246abd449fa3e244ac94aead98d9983dff415
Hannes Preishuber <hannesp@ppedv.de>
2019-08-29 08:52:32 +0200

    DataTable Alt Command Button

---
+++ WebformsMuc2019CS/Modul08/DataDatableAlt.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" 
+    
+    MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="DataDatableAlt.aspx.cs" Inherits="WebformsMuc2019CS.Modul07.DataDatableAlt" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <ul>
+        <asp:Listview ID="ListView1" runat="server" DataKeyNames="CustomerID" 
+            OnItemCommand="ListView1_ItemCommand">
+            <ItemTemplate>
+                <li><%#Eval("ContactName") %> <asp:Button ID="Button1" runat="server" Text="del" 
+                    CommandArgument='<%# Container.DataItemIndex %>'
+                    CommandName="hannes" /></li>
+
+            </ItemTemplate>
+        </asp:Listview>
+    </ul>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ScriptPlaceHolder1" runat="server">
+</asp:Content>
---
+++ WebformsMuc2019CS/Modul08/DataDatableAlt.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsMuc2019CS.Modul07
+{
+    public partial class DataDatableAlt : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (SqlConnection con = new SqlConnection(
+               ConfigurationManager.ConnectionStrings["NorthwindConnectionString1"].ConnectionString))
+                {
+                    var cmd = new SqlCommand("select * from Customers ", con);
+                    var da = new SqlDataAdapter(cmd);
+                    var dt = new DataTable();
+                    con.Open();
+                    da.Fill(dt);
+
+                    ListView1.DataSource
+                            = dt;
+                }
+                ListView1.DataBind();
+            }
+
+        }
+
+        protected void ListView1_ItemCommand(object sender, ListViewCommandEventArgs e)
+        {
+            switch (e.CommandName)
+            {
+                case "hannes":
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        
+    }
+}
