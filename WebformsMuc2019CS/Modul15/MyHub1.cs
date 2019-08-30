e0ab41b5b81c77f836f1818be4f7d8b1af3de13b
Hannes Preishuber <hannesp@ppedv.de>
2019-08-30 15:14:35 +0200

    SignalR Chat

---
+++ WebformsMuc2019CS/Modul15/MyHub1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebformsMuc2019CS.Modul15
+{
+    public class MyHub1 : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        public void CallMsg(string msg)
+        {
+            Clients.All.UpdateMsgs($"{DateTime.Now.ToShortDateString()}:{msg}");
+        }
+    }
+}
